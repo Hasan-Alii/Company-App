678d6bd00fe9bbbe3c90d78c5d71df2430000e17
Hasan Ali <hasan.aly.dev@gmail.com>
2025-07-24 00:11:43 +0300

    Reapply "commit", Some changes
    This reverts commit 812a70d1966c5189d36cf1f0607a40d8856e491e.

---
+++ Company.G02.PL/Helper/DocumentSettings.cs
@@ -0,0 +1,43 @@
+﻿namespace Company.G02.PL.Helper
+{
+    public static class DocumentSettings
+    {
+        // Upload
+
+        public static string Upload(IFormFile file, string folderName)
+        {
+            // 1. GetAsync Location Of Folder
+
+            // string folderPath = $"C:\\Users\\Mcpex\\source\\repos\\Company.G02 Solution\\Company.G02.PL\\wwwroot\\files\\{folderName}";
+
+            string folderPath =  Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot\\files\\{folderName}");
+
+            // 2. GetAsync File Name And Make It Unique
+
+            string fileName = $"{Guid.NewGuid()}{file.FileName}";
+
+            // 3. GetAsync File Path: FolderPath + FileName
+
+            string filePath = Path.Combine(folderPath, fileName);
+
+            // 4. Create Object Of Type File Stream (Data per Second)
+
+            // unmanaged resource requires opening then closing the connection
+            using var FileStream = new FileStream(filePath, FileMode.Create);
+
+            file.CopyTo(FileStream);
+
+            return fileName;
+        }
+
+        // Delete
+
+        public static void Delete(string fileName, string folderName)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot\\files\\{folderName}", fileName);
+
+            if(File.Exists(filePath))
+                File.Delete(filePath);
+        }
+    }
+}
